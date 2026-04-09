f9897fc0b639e23130e9bd5bc9117711bdb23cad
Gian-Cara <gianluca.caracristi@mat.tn.it>
2026-04-09 13:53:11 +0000

    Inizializzazione progetto

---
+++ catalogocanzoni/src/main/java/itt/marconi/catalogocanzoni/CatalogocanzoniApplication.java
@@ -0,0 +1,13 @@
+package itt.marconi.catalogocanzoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogocanzoniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogocanzoniApplication.class, args);
+	}
+
+}
---
+++ catalogocanzoni/src/test/java/itt/marconi/catalogocanzoni/CatalogocanzoniApplicationTests.java
@@ -0,0 +1,13 @@
+package itt.marconi.catalogocanzoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogocanzoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
